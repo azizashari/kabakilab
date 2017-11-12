28c4ca74d3e6b96a27c6889676ab590495c7214c
azizashari <aziz.ashari1@gmail.com>
2017-11-12 07:33:27 +0700

    second - katalog1

--- app/src/main/java/mazizashari/kabakilab/Petunjuk.java
+++ app/src/main/java/mazizashari/kabakilab/Petunjuk.java
@@ -1,7 +1,13 @@
 package mazizashari.kabakilab;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+
+import mazizashari.kabakilab.Katalog.katalog;
 
 public class Petunjuk extends AppCompatActivity {
 
@@ -9,5 +15,14 @@ public class Petunjuk extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_petunjuk);
+    }
+
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            Intent i = new Intent(this, katalog.class);
+            startActivity(i);
+            finish();
+        }
+        return false;
     }
 }
